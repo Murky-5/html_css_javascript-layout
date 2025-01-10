87cd0ad0f70a75c8b11ffb705474c1b83fe1925c
MirkoDD <mirkodidio111@gmail.com>
2025-01-10 17:40:09 +0100

    fixed:data_catches

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,8 +1,8 @@
 const cardElm = document.getElementById("card-container")
-
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
 .then(({data}) => {
-    for (let i = 1; i <= id; i++) {
+    for (let i = 0; i < data.length; i++) {
+        let {thumbnailUrl, id, title} = data[i]
         cardElm.innerHTML += `   
         <div class="card">
             <div class="border-card p-5">
@@ -14,8 +14,5 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
         </div>
         `
     }
-    console.log(data, data.title, data.thumbnailUrl)
 })
-.catch(() => {
-})
-console.log(axios.length)
+
