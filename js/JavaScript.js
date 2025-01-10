1aafe33c21eafbb02fb43a88a48cbb52bfff7723
MirkoDD <mirkodidio111@gmail.com>
2025-01-10 17:01:48 +0100

    sixth

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -3,7 +3,7 @@ const cardElm = document.getElementById("card-container")
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
 .then(({data}) => {
     for (let i = 1; i <= id; i++) {
-        cardElm.innerHTML += `  
+        cardElm.innerHTML += `   
         <div class="card">
             <div class="border-card p-5">
                 <div class="img">
