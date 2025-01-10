3dabf999d5f7d8bbe6116a4c5825501187d3910a
MirkoDD <mirkodidio111@gmail.com>
2025-01-10 16:29:31 +0100

    first

---
+++ js/JavaScript.js
@@ -0,0 +1,21 @@
+const cardElm = document.getElementById("card-container")
+
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+.then(({data}) => {
+    for (let i = 1; i <= id; i++) {
+        cardElm.innerHTML += `
+        <div class="card">
+            <div class="border-card p-5">
+                <div class="img">
+                    <img src="${thumbnailUrl}" alt="${id}">
+                </div>
+                <p>${title}</p>
+            </div>
+        </div>
+        `
+    }
+    console.log(data, data.title, data.thumbnailUrl)
+})
+.catch(() => {
+})
+console.log(axios.length)
